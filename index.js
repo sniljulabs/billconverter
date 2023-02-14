13dcadb1be1974c1c4ea1fc4584f700ef10acee3
sniljulabs <124678143+sniljulabs@users.noreply.github.com>
2023-02-14 16:45:48 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ function submitform() {
   let unit = document.getElementById("units").value;
   let amt = 0;
   if (unit > 0 && unit <= 20) {
+    //0-20
     amt = 75 + unit * 5;
   } else if (unit >= 21 && unit <= 30) {
-    amt = 200 + (unit - 20) * 7;
+    //21-30
+    amt = 200 + (unit - 20) * 6.5;
   } else if (unit >= 31 && unit <= 50) {
-    amt = 295 + (unit - 30) * 8.5;
-  } else if (unit >= 51 && unit <= 150) {
-    amt = 490 + (unit - 50) * 10;
-  } else if (unit >= 151 && unit <= 250) {
-    amt = 1515 + (unit - 150) * 11;
-  } else if (unit >= 251 && unit <= 400) {
-    amt = 2640 + (unit - 250) * 12;
-  } else if (unit > 400) {
-    amt = 4465 + (unit - 400) * 13;
+    //31-50
+    amt = 265 + (unit - 30) * 8;
+  } else if (unit >= 51 && unit <= 100) {
+    //51-100
+    amt = 450 + (unit - 50) * 9.5;
+  } else if (unit >= 101 && unit <= 250) {
+    //101-250
+    amt = 950 + (unit - 100) * 11;
+  } else if (unit > 250) {
+    //250 above
+    amt = 2425 + (unit - 250) * 11;
   } else {
     alert("Error!");
   }
